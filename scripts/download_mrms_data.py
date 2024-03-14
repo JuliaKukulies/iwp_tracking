2baec851b79fc476f8a1f1055541e631c276e069
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2024-03-13 21:10:01 -0600

    Handle errors from MRMS download.

--- scripts/download_mrms_data.py
+++ scripts/download_mrms_data.py
@@ -39,47 +39,53 @@ def download_mrms_data(start_time: str, end_time: str, output_folder: str) -> No
     while time <= end_time:
         LOGGER.info("Processing '%s'", time)
 
-        mrms_precip_rate_rec = mrms.precip_rate.find_files(TimeRange(time, time))
-        mrms_rqi_rec = mrms.radar_quality_index.find_files(TimeRange(time, time))
-        mrms_precip_flag_rec = mrms.precip_flag.find_files(TimeRange(time, time))
+        try:
+            mrms_precip_rate_rec = mrms.precip_rate.find_files(TimeRange(time, time))
+            mrms_rqi_rec = mrms.radar_quality_index.find_files(TimeRange(time, time))
+            mrms_precip_flag_rec = mrms.precip_flag.find_files(TimeRange(time, time))
 
-        if (
-            len(mrms_precip_rate_rec) == 0
-            or len(mrms_precip_flag_rec) == 0
-            or len(mrms_rqi_rec) == 0
-        ):
-            LOGGER.warning("Missing MRMS files for %s.", time)
-        mrms_precip_rate_rec = mrms_precip_rate_rec[0].get()
-        mrms_precip_rate = mrms.precip_rate.open(mrms_precip_rate_rec)
-        mrms_precip_rate = resample_scalar(mrms_precip_rate.precip_rate, CONUS_GRID)
+            if (
+                len(mrms_precip_rate_rec) == 0
+                or len(mrms_precip_flag_rec) == 0
+                or len(mrms_rqi_rec) == 0
+            ):
+                LOGGER.warning("Missing MRMS files for %s.", time)
+            mrms_precip_rate_rec = mrms_precip_rate_rec[0].get()
+            mrms_precip_rate = mrms.precip_rate.open(mrms_precip_rate_rec)
+            mrms_precip_rate = resample_scalar(mrms_precip_rate.precip_rate, CONUS_GRID)
 
-        mrms_rqi_rec = mrms_rqi_rec[0].get()
-        mrms_rqi = mrms.radar_quality_index.open(mrms_rqi_rec)
-        mrms_rqi = resample_scalar(mrms_rqi.radar_quality_index, CONUS_GRID)
+            mrms_rqi_rec = mrms_rqi_rec[0].get()
+            mrms_rqi = mrms.radar_quality_index.open(mrms_rqi_rec)
+            mrms_rqi = resample_scalar(mrms_rqi.radar_quality_index, CONUS_GRID)
 
-        mrms_precip_flag_rec = mrms_precip_flag_rec[0].get()
-        mrms_precip_flag = mrms.precip_flag.open(mrms_precip_flag_rec)
-        mrms_precip_flag = resample_categorical(
-            mrms_precip_flag.precip_flag, CONUS_GRID
-        )
+            mrms_precip_flag_rec = mrms_precip_flag_rec[0].get()
+            mrms_precip_flag = mrms.precip_flag.open(mrms_precip_flag_rec)
+            mrms_precip_flag = resample_categorical(
+                mrms_precip_flag.precip_flag, CONUS_GRID
+            )
 
-        date = to_datetime(time)
-        date_str = date.strftime("%Y%m%d_%H_%M")
+            date = to_datetime(time)
+            date_str = date.strftime("%Y%m%d_%H_%M")
 
-        filename = f"mrms_{date_str}.nc"
-        results = xr.Dataset(
-            {
-                "precip_rate": mrms_precip_rate,
-                "precip_flag": mrms_precip_flag,
-                "rqi": mrms_rqi,
+            filename = f"mrms_{date_str}.nc"
+            results = xr.Dataset(
+                {
+                    "precip_rate": mrms_precip_rate,
+                    "precip_flag": mrms_precip_flag,
+                    "rqi": mrms_rqi,
+                }
+            )
+
+            encoding = {
+                "precip_rate": {"dtype": "float32", "zlib": True},
+                "precip_flag": {"dtype": "int8", "zlib": True},
             }
-        )
+            results.to_netcdf(output_folder / filename, encoding=encoding)
 
-        encoding = {
-            "precip_rate": {"dtype": "float32", "zlib": True},
-            "precip_flag": {"dtype": "int8", "zlib": True},
-        }
-        results.to_netcdf(output_folder / filename, encoding=encoding)
+        except Exception:
+            LOGGER.exception(
+                "The following error was ecountered when processing time %s."
+            )
 
         time += np.timedelta64(30, "m")
 
