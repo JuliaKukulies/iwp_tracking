2f9dd37d143792e9d01d9922cb496be4aef2d852
Julia Kukulies <julia.kukulies@gu.se>
2024-03-23 16:33:41 -0600

    update tracking scripts

--- scripts/ccic_tracking.py
+++ scripts/ccic_tracking.py
@@ -31,13 +31,12 @@ months =  sys.argv[1:]
 dxy, dt  = 4000, 1800
 
 ################################ parameters for feature detection ####################################################
-
-optimal_threshold = 0.18
-dc_threshold = 1.0 
+optimal_threshold = 0.18 
+dc_threshold = 1.0
 
 # parameters for feature detection                                                           
 parameters_features = {}
-parameters_features['threshold']=[optimal_threshold, dc_threshold] #thresholds for ice water path 
+parameters_features['threshold']=[optimal_threshold] #thresholds for ice water path 
 parameters_features['target']='maximum'
 parameters_features['n_min_threshold']= 100 # minimum number of grid cells that need to be above specified thresholds 
 parameters_features['statistic'] = {"feature_min_iwp": np.nanmin, 'feature_max_iwp': np.nanmax, 'feature_mean_iwp': np.nanmean}
@@ -52,9 +51,15 @@ parameters_linking['method_linking']= 'predict'
 
 # parameters for segmentation 
 parameters_segmentation = {}
-parameters_segmentation['threshold']= optimal_threshold+ 0.1 # kg/m2 used to define the extent of the cloud objects                  
+parameters_segmentation['threshold']= optimal_threshold + 0.1 # kg/m2 used to define the extent of the cloud objects                  
 parameters_segmentation['target'] = "maximum"
 parameters_segmentation['statistic'] = {"object_min_iwp": np.nanmin, 'object_max_iwp': np.nanmax, 'object_mean_iwp': np.nanmean}
+
+# for DC thresholds
+parameters_segmentation_dc = parameters_segmentation.copy()
+parameters_features_dc = parameters_features.copy()
+parameters_segmentation_dc['threshold']= dc_threshold + 0.1
+parameters_features_dc['threshold']= dc_threshold
 
 ############################# main tracking program ########################### #################################################
 year = 2020
@@ -65,11 +70,11 @@ for month in months:
     # check first if day has already been processed
     track_file = Path(savedir /  ('tracks_iwp_'+ str(year) + str(month).zfill(2) +'.nc'))
     # if no track file for this month exist yet, start the tracking procedure 
-
     if track_file.is_file() is False:
         nr_days = calendar.monthrange(int(year), int(month))[1]
         days = np.arange(1, nr_days + 1)
         feature_df_list = [] # list to store daily feature data frames
+        features_df_list_dc = []
         
         for day in days:
             # read in global data and relevant variables for one day  
@@ -90,33 +95,41 @@ for month in months:
             print(datetime.now(), f"Commencing segmentation IWP", flush=True)
             mask, features = tobac.segmentation_2D(features_d, iwp_iris, dxy, **parameters_segmentation)
             # convert output tracks to xarray and save them to daily tracks for segmentation 
-            features_threshold = tracks.set_index(features.feature).to_xarray()
+            features = features.set_index(features.feature).to_xarray()
             # save daily mask and feature files
             xr.DataArray.from_iris(mask).to_netcdf(savedir / ('mask_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) +  '.nc' ))
             features.to_netcdf(savedir /  ('features_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '.nc'))
 
-            
+
+            # same for DC threshold
+            features_dc=tobac.feature_detection_multithreshold(iwp_iris ,dxy, **parameters_features_dc)
+            # save daily features in list to use for a monthly tracking 
+            feature_df_list_dc.append(features_dc)
+            mask_dc, features_dc = tobac.segmentation_2D(features_dc, iwp_iris, dxy, **parameters_segmentation_dc)
+            # convert output tracks to xarray and save them to daily tracks for segmentation 
+            features_dc = features_dc.set_index(features_dc.feature).to_xarray()
+            # save daily mask and feature files
+            xr.DataArray.from_iris(mask_dc).to_netcdf(savedir / ('mask_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) +  '_dc.nc' ))
+            features_dc.to_netcdf(savedir /  ('features_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_dc.nc'))
+
         # Combine all feature dataframes for one month
         features = tobac.utils.combine_tobac_feats(feature_df_list)
-
+        features_dc = tobac.utils.combine_tobac_feats(feature_df_list_dc)
+        
         ### Perform tracking on monthly basis ###
         print(datetime.now(), f"Commencing tracking IWP", flush=True)
         tracks = tobac.linking_trackpy(features, iwp_iris, dt, dxy, **parameters_linking)
-        tracks = tracks[tracks.cell != -1]
-
         # reduce tracks to valid cells and those cells that contain deep convection
         tracks = tracks[tracks.cell != -1]
-        tracks_convective = tracks.groupby("cell").feature_max_iwp.max()
-        valid_cells = tracks_convective.index[tracks_convective >= dc_threshold]
-        tracks = tracks[np.isin(tracks.cell, valid_cells)]
         tracks.to_xarray().to_netcdf(track_file)
+
+        tracks_dc = tobac.linking_trackpy(features_dc, iwp_iris, dt, dxy, **parameters_linking)
+        # reduce tracks to valid cells and those cells that contain deep convection
+        tracks_dc = tracks[tracks.cell != -1]
+        tracks_dc.to_xarray().to_netcdf( Path(savedir / ('tracks_iwp_'+ str(year) + str(month).zfill(2) +'_dc.nc')))
 
     # rerun the segmentation, but this time on the storm tracks instead of all features 
     if track_file.is_file() is True and segmentation_flag is True:
-        nr_days = calendar.monthrange(int(year), int(month))[1]
-        days = np.arange(1, nr_days + 1)
-        tracks = xr.open_dataset(track_file).to_dataframe()
-        tracks_threshold = []
         
         ### segmentation on daily basis only for tracked storm objects ###
         for day in days:
@@ -130,18 +143,25 @@ for month in months:
             
             # segmentation IWP threshold  
             print(datetime.now(), f"Commencing segmentation IWP", flush=True)
-            mask, tracks = tobac.segmentation_2D(tracks, iwp_iris, dxy, **parameters_segmentation)
-            
+            mask, tracks_day = tobac.segmentation_2D(tracks, iwp_iris, dxy, **parameters_segmentation)            
             # convert output tracks to xarray and save them to daily tracks for segmentation 
-            tracks_threshold = tracks.set_index(tracks.feature).to_xarray() 
+            tracks_day= tracks_d.set_index(tracks.feature).to_xarray() 
 
             # save daily mask and track files 
             xr.DataArray.from_iris(mask).to_netcdf(savedir / ('mask_storm_tracks_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '.nc'))
-            tracks_threshold.to_netcdf(savedir /  ('features_storm_tracks_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '.nc'))
-            print(datetime.now(), str("Processing finished for " + str(year)+ str(month).zfill(2)) + str(day).zfill(2) , flush=True)
+            tracks_day.to_netcdf(savedir /  ('features_storm_tracks_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '.nc'))
 
+            # same for DC threshold   
+            mask_dc, tracks_dc_day = tobac.segmentation_2D(tracks_dc, iwp_iris, dxy, **parameters_segmentation_dc)
+            tracks_dc_day = tracks_dc_day.set_index(tracks.feature).to_xarray() 
+            # save daily mask and track files 
+            xr.DataArray.from_iris(mask_dc).to_netcdf(savedir / ('mask_storm_tracks_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_dc.nc'))
+            tracks_threshold_dc.to_netcdf(savedir /  ('features_storm_tracks_iwp_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_dc.nc'))
+            print(datetime.now(), str("Processing finished for " + str(year)+ str(month).zfill(2)) + str(day).zfill(2) , flush=True)
+            
             ds.close()
-            tracks_threshold.close()
+            tracks_day.close()
+            tracks_dc_day.close()
     else:
         print(str(track_file) , '  already processed.', flush = True )
         continue
--- scripts/ir_tracking.py
+++ scripts/ir_tracking.py
@@ -1,7 +1,7 @@
 """
 
-Tracking convection based on IR brightness temperatures in the CPC-IR dataset.                                  
-     
+Tracking convection based on IR brightness temperatures in the CPC-IR dataset.
+
 ---------------------------------
 Email contact: kukulies@ucar.edu
 ---------------------------------
@@ -20,6 +20,9 @@ import ccic
 import warnings
 warnings.filterwarnings("ignore")
 
+print('Using tobac version', tobac.__version__)
+
+
 # data paths                                                                                                    
 data_path = Path('/glade/campaign/mmm/c3we/prein/observations/GPM_MERGIR/data/2020/')
 savedir = Path('/glade/derecho/scratch/kukulies/ccic/tracking/tb/')
@@ -32,12 +35,12 @@ dxy, dt  = 4000, 1800
 
 # define thresholds 
 conventional_threshold = 241 
-optimal_threshold = 230
+optimal_threshold = 256
 dc_threshold = 220
 
 # parameters for feature detection                                                           
 parameters_features = {}
-parameters_features['threshold']=[conventional_threshold, dc_threshold] #thresholds for ice water path 
+parameters_features['threshold']=[conventional_threshold] #thresholds for ice water path 
 parameters_features['target']='minimum'
 parameters_features['n_min_threshold']= 100 # minimum number of grid cells that need to be above specified thresholds 
 parameters_features['statistic'] = {"feature_min_tb": np.nanmin, 'feature_max_tb': np.nanmax, 'feature_mean_tb': np.nanmean}
@@ -58,9 +61,14 @@ parameters_segmentation['statistic'] = {"object_min_tb": np.nanmin, 'object_max_
 
 # optimal thresholds 
 parameters_features_optimal = parameters_features.copy()
-parameters_featues_optimal['threshold'] = [optimal_threshold, dc_threshold]
-parameters_segmentation_optimal = parameters_segmentation_optimal.copy()
-parameters_segmentation_optimal['threshold'] = optimal_threshold + 1 
+parameters_features_optimal['threshold'] = [optimal_threshold]
+parameters_segmentation_optimal = parameters_segmentation.copy()
+parameters_segmentation_optimal['threshold'] = optimal_threshold + 1
+
+parameters_features_dc = parameters_features.copy()
+parameters_features_dc['threshold'] = [dc_threshold]
+parameters_segmentation_dc = parameters_segmentation.copy()
+parameters_segmentation_dc['threshold'] = dc_threshold + 1 
 
 ############################# main tracking program ########################### #################################################
 year = 2020
@@ -77,6 +85,7 @@ for month in months:
         days = np.arange(1, nr_days + 1)
         feature_df_list = [] # list to store daily feature data frames
         feature_df_list_opt = []
+        feature_df_list_dc = []
         
         for day in days:
             # read in global data and relevant variables for one day
@@ -84,7 +93,9 @@ for month in months:
             ds = xr.open_mfdataset(fnames)
             # field used for tracking: total ice water path                                                     
             tb = ds.Tb
-            # interpolate nan values in Tb data 
+            # interpolate nan values in Tb data
+            rechunked = ds.Tb.chunk(dict(lat=-1))
+            tb = rechunked.interpolate_na(dim='lat', method='linear')
             
             # convert tracking field to iris                                                                    
             tb_iris = tb.to_iris()
@@ -99,12 +110,17 @@ for month in months:
             features_opt = tobac.feature_detection_multithreshold(tb_iris ,dxy, **parameters_features_optimal)
             # save daily features in list to use for a monthly tracking 
             feature_df_list_opt.append(features_opt)
+
+            # run the feature detection on daily file with optimal threshold 
+            features_dc = tobac.feature_detection_multithreshold(tb_iris ,dxy, **parameters_features_dc)
+            # save daily features in list to use for a monthly tracking 
+            feature_df_list_dc.append(features_dc)
             
             # run the segmentation on daily file  
             print(datetime.now(), f"Commencing segmentation  ", flush=True)
             mask, features = tobac.segmentation_2D(features_d, tb_iris, dxy, **parameters_segmentation)
             # convert output tracks to xarray and save them to daily tracks for segmentation 
-            features_threshold = tracks.set_index(features.feature).to_xarray()
+            features = features.set_index(features.feature).to_xarray()
             # save daily mask and feature files
             xr.DataArray.from_iris(mask).to_netcdf(savedir / ('mask_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) +  '.nc' ))
             features.to_netcdf(savedir /  ('features_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '.nc'))
@@ -112,44 +128,48 @@ for month in months:
             # segmentation for optimal threshold
             mask_opt, features_opt = tobac.segmentation_2D(features_opt, tb_iris, dxy, **parameters_segmentation_optimal)
             # convert output tracks to xarray and save them to daily tracks for segmentation 
-            features_threshold_opt = tracks.set_index(features_opt.feature).to_xarray()
+            features_opt = features_opt.set_index(features_opt.feature).to_xarray()
             # save daily mask and feature files
             xr.DataArray.from_iris(mask_opt).to_netcdf(savedir / ('mask_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) +  '_opt.nc' ))
             features_opt.to_netcdf(savedir /  ('features_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_opt.nc'))
-            
+
+
+            # segmentation for optimal threshold
+            mask_dc, features_dc = tobac.segmentation_2D(features_dc, tb_iris, dxy, **parameters_segmentation_dc)
+            # convert output tracks to xarray and save them to daily tracks for segmentation 
+            features_dc = features_dc.set_index(features_dc.feature).to_xarray()
+            # save daily mask and feature files
+            xr.DataArray.from_iris(mask_dc).to_netcdf(savedir / ('mask_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) +  '_dc.nc' ))
+            features_dc.to_netcdf(savedir /  ('features_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_dc.nc'))
+
         # Combine all feature dataframes for one month
         features = tobac.utils.combine_tobac_feats(feature_df_list)
         features_opt = tobac.utils.combine_tobac_feats(feature_df_list_opt)
-
+        features_dc = tobac.utils.combine_tobac_feats(feature_df_list_dc)
+        
         ### Perform tracking on monthly basis ###
         print(datetime.now(), f"Commencing tracking  ", flush=True)
         tracks = tobac.linking_trackpy(features, tb_iris, dt, dxy, **parameters_linking)
         # reduce tracks to valid cells and those cells that contain deep convection
         tracks = tracks[tracks.cell != -1]
-        tracks_convective = tracks.groupby("cell").feature_max_tb.max()
-        valid_cells = tracks_convective.index[tracks_convective >= dc_threshold]
-        tracks = tracks[np.isin(tracks.cell, valid_cells)]
         tracks.to_xarray().to_netcdf(track_file)
-
 
         ### Perform tracking on monthly basis for optimal thresholds ###
         print(datetime.now(), f"Commencing tracking  ", flush=True)
         tracks_opt = tobac.linking_trackpy(features_opt, tb_iris, dt, dxy, **parameters_linking)
         # reduce tracks to valid cells and those cells that contain deep convection
         tracks_opt = tracks[tracks_opt.cell != -1]
-        tracks_convective = tracks_opt.groupby("cell").feature_max_tb.max()
-        valid_cells = tracks_convective.index[tracks_convective >= dc_threshold]
-        tracks_opt = tracks[np.isin(tracks_opt.cell, valid_cells)]
         tracks_opt.to_xarray().to_netcdf(Path(savedir /  ('tracks_tb_'+ str(year) + str(month).zfill(2) +'_opt.nc')))
 
+        ### Perform tracking on monthly basis for DC thresholds ###
+        print(datetime.now(), f"Commencing tracking  ", flush=True)
+        tracks_dc = tobac.linking_trackpy(features_dc, tb_iris, dt, dxy, **parameters_linking)
+        # reduce tracks to valid cells and those cells that contain deep convection
+        tracks_dc = tracks[tracks_dc.cell != -1]
+        tracks_dc.to_xarray().to_netcdf(Path(savedir /  ('tracks_tb_'+ str(year) + str(month).zfill(2) +'_dc.nc')))
 
-        
-    # rerun the segmentation, but this time on the storm tracks instead of all features 
+    # rerun the segmentations, but this time on the storm tracks instead of all features 
     if track_file.is_file() is True and segmentation_flag is True:
-        nr_days = calendar.monthrange(int(year), int(month))[1]
-        days = np.arange(1, nr_days + 1)
-        tracks = xr.open_dataset(track_file).to_dataframe()
-        tracks_threshold = []
         
         ### segmentation on daily basis only for tracked storm objects ###
         for day in days:
@@ -158,33 +178,45 @@ for month in months:
             ds = xr.open_mfdataset(fnames)
             # field used for tracking: total ice water path                                                     
             tb = ds.Tb
+            # interpolate nan values
+            rechunked = ds.Tb.chunk(dict(lat=-1))
+            tb  = rechunked.interpolate_na(dim='lat', method='linear') 
             # convert tracking field to iris                                                                    
             tb_iris = tb.to_iris()
             
             # segmentation threshold  
             print(datetime.now(), f"Commencing segmentation", flush=True)
-            mask, tracks = tobac.segmentation_2D(tracks, tb_iris, dxy, **parameters_segmentation)    
+            mask, tracks_day = tobac.segmentation_2D(tracks, tb_iris, dxy, **parameters_segmentation)    
             # convert output tracks to xarray and save them to daily tracks for segmentation 
-            tracks_threshold = tracks.set_index(tracks.feature).to_xarray() 
-
+            tracks_day = tracks_day.set_index(tracks_day.feature).to_xarray() 
             # save daily mask and track files 
             xr.DataArray.from_iris(mask).to_netcdf(savedir / ('mask_storm_tracks_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '.nc'))
-            tracks_threshold.to_netcdf(savedir /  ('features_storm_tracks_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '.nc'))
+            tracks_day.to_netcdf(savedir /  ('features_storm_tracks_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '.nc'))
 
             # segmentation threshold for optimal threshold
-            print(datetime.now(), f"Commencing segmentation", flush=True)
-            mask_opt, tracks_opt = tobac.segmentation_2D(tracks_opt, tb_iris, dxy, **parameters_segmentation_optimal)    
+            mask_opt, tracks_opt_day = tobac.segmentation_2D(tracks_opt, tb_iris, dxy, **parameters_segmentation_optimal)    
             # convert output tracks to xarray and save them to daily tracks for segmentation 
-            tracks_threshold_opt = tracks_opt.set_index(tracks_opt.feature).to_xarray() 
+            tracks_opt_day = tracks_opt_day.set_index(tracks_opt_day.feature).to_xarray() 
             # save daily mask and track files 
             xr.DataArray.from_iris(mask_opt).to_netcdf(savedir / ('mask_storm_tracks_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_opt.nc'))
-            tracks_threshold_opt.to_netcdf(savedir /  ('features_storm_tracks_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_opt.nc'))
+            tracks_opt_day.to_netcdf(savedir /  ('features_storm_tracks_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_opt.nc'))
+
+            # segmentation threshold for DC threshold
+            mask_dc, tracks_dc_day = tobac.segmentation_2D(tracks_dc, tb_iris, dxy, **parameters_segmentation_dc)    
+            # convert output tracks to xarray and save them to daily tracks for segmentation 
+            tracks_dc_day = tracks_dc_day.set_index(tracks_dc_day.feature).to_xarray() 
+            # save daily mask and track files 
+            xr.DataArray.from_iris(mask_dc).to_netcdf(savedir / ('mask_storm_tracks_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_dc.nc'))
+            tracks_opt_day.to_netcdf(savedir /  ('features_storm_tracks_tb_'+ str(year) + str(month).zfill(2) + str(day).zfill(2) + '_dc.nc'))
+
+
             print(datetime.now(), str("Processing finished for " + str(year)+ str(month).zfill(2)) + str(day).zfill(2) , flush=True)
             ds.close()
-            tracks_threshold.close()
-            tracks_threshold_opt.close()
+            tracks_day.close()
+            tracks_opt_day.close()
+            tracks_dc_day.close()                                     
     else:
-        print(str(track_file) , '  already processed.', flush = True )
+        print(str(track_file) , '  already processed.', flush = True)
         continue
 
 
